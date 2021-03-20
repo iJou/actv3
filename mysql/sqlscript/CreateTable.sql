cc88cd3a2effab15a6fdff2b6ebbbbca9b4f12a3
iJou <aikoncap.std@gmail.com>
2021-03-20 13:23:10 -0700

    initial

---
+++ mysql/sqlscript/CreateTable.sql
@@ -0,0 +1,12 @@
+CREATE TABLE material(
+idprod varchar(40) primary key not null, 
+tmat varchar(20), 
+name varchar(30), 
+descr varchar(50)
+;
+
+CREATE TABLE stock(
+idprod varchar(40) primary key not null, 
+store varchar(20),
+qty int
+);
