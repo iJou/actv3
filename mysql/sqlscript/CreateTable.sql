869e8e104e76b78f789b30ad68dddfba63db07d4
iJou <aikoncap.std@gmail.com>
2021-03-22 11:58:31 -0700

    insertsql

--- mysql/sqlscript/CreateTable.sql
+++ mysql/sqlscript/CreateTable.sql
@@ -6,7 +6,8 @@ descr varchar(50)
 );
 
 CREATE TABLE stock(
-idprod varchar(40) primary key not null, 
-store varchar(20) primary key not null, 
+idprod varchar(40) not null, 
+store varchar(20) not null, 
+PRIMARY KEY(idprod,store),
 qty int
 );
